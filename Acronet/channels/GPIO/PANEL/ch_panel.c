12730cecb2bc5f36f1ee0a1b4cf786521dbb0518
fabio <fabio@acrotec.it>
2018-11-29 17:15:43 +0100

    last commit

---
+++ Acronet/channels/GPIO/PANEL/ch_panel.c
@@ -0,0 +1,21 @@
+/*
+ * ACRONET Project
+ * http://www.acronet.cc
+ *
+ * Copyright ( C ) 2014 Acrotec srl
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the EUPL v.1.1 license.  See http://ec.europa.eu/idabc/eupl.html for details.
+ */
+
+#include "Acronet/setup.h"
+#include "Acronet/HAL/hal_interface.h"
+#include <asf.h>
+#include <stdio.h>
+#include "progmem.h"
+#include <conf_board.h>
+#include <conf_usart_serial.h>
+
+
+#include "Acronet/globals.h"
---
+++ Acronet/channels/GPIO/PANEL/ch_panel.h
@@ -0,0 +1,20 @@
+/*
+ * ACRONET Project
+ * http://www.acronet.cc
+ *
+ * Copyright ( C ) 2014 Acrotec srl
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the EUPL v.1.1 license.  See http://ec.europa.eu/idabc/eupl.html for details.
+ */
+
+
+#ifndef CH_PANEL_H_
+#define CH_PANEL_H_
+
+
+
+
+
+#endif /* CH_PANEL_H_ */
